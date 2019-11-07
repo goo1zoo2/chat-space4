7f0b772eff969bebf02770667f114b007b171c32
せきね <dmmu-wieshaft-beto1@ezweb.ne.jp>
2019-11-07 11:59:35 +0900

    group-edit-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,13 @@
 class MessagesController < ApplicationController
+
+  
   def index
-    @message = "プログラミング"
+   
+
   end
 
   def create
     Message.create(name: "", image: "", text: "")
   end
+
 end
