c2c55e4d407dfd9e0fc759aaab7bd7ca8e47dc43
せきね <dmmu-wieshaft-beto1@ezweb.ne.jp>
2019-11-09 07:58:32 +0900

    send-message-function2

--- db/migrate/20191103123817_create_messages.rb
+++ db/migrate/20191103123817_create_messages.rb
@@ -1,11 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.references :group, foreign_key: true
-      t.references :user, foreign_key: true
       t.text       :content
       t.string     :image
-      t.timestamps null: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps 
     end
   end
 end
