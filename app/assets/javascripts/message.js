8aa451da6cf6ec9523e2de5038a14b6a2732ef4f
せきね <dmmu-wieshaft-beto1@ezweb.ne.jp>
2019-11-28 01:13:46 +0900

    automatic-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,31 @@
 $(function(){
   function buildHTML(message){
     var content = message.content ? `${message.content}` : "";
-    var image = message.image ? `<img src= ${message.image}>` : "";
-    var html = `<div class="message">
-                  <div class="message__upper-info">
-                    <p class="message__upper-info__talker">
-                      ${message.user_name}
-                    </p>
-                    <p class="message__upper-info__date">
-                      ${message.date}
-                    </p>
-                  </div>
-                  <p class="message__text">
-                    ${content}
-                    <div class="message__text__image">
-                    ${image}
-                    </div>
-                  </p>
-                </div>`
+    var image = message.image ? `<img src= "${message.image}">` : "";
+    var html = `
+          <div class="message" data-id="${message.id}">
+            <div class="message__upper-info">
+              <p class="message__upper-info__talker">
+                ${message.user_name}
+              </p>
+              <p class="message__upper-info__date">
+                ${message.date}
+              </p>
+            </div>
+            <p class="message__text">
+              ${content}
+            </p>
+            <div class="image">
+              ${image}
+            </div>
+          </div>
+          `
     return html;
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -33,6 +36,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
@@ -42,5 +46,28 @@ $(function(){
     .fail(function(){
       alert("メッセージ送信に失敗しました");
     })
-   })
+  })
+  
+  var reloadMessages = function(){
+    var last_message_id = $('.message:last').data("id");
+    $.ajax({ //ajax通信で以下のことを行う
+      url: './api/messages',
+      type: 'GET',
+      dataType: 'json', 
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+      })
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
