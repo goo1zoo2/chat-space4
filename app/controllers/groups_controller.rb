1c82fbdebd63b9c12f37a278e11a611022a2d085
せきね <dmmu-wieshaft-beto1@ezweb.ne.jp>
2020-07-04 16:12:21 +0900

    変更
    puma
    localhost:3000->3500

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,15 @@ class GroupsController < ApplicationController
   end
 
   def new
+    # binding.pry
     @group = Group.new
     @group.users << current_user
+
   end
 
   def create
     @group = Group.new(group_params)
+    # binding.pry
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
